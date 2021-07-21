64e909d1a7d055ed5b7dd163c2ca1e6c80fd3a20
Boye-dev <oyelolaboye@gmail.com>
2021-07-21 21:11:40 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="head">
+          <p className="join">Join our community</p>
+          <p className="guarant"> 30-day, hassle-free money back guarantee </p>
+          <p className="gain">
+            Gain access to our full library of tutorials along with expert code
+            reviews. Perfect for any developers who are serious about honing
+            their skills.
+          </p>
+        </div>
+        <div className="foot1">
+          <p className="monthly"> Monthly Subscription</p>
+          <p className="per">
+            <span className="dollar">$29</span> per month
+          </p>
+          <p className="access">Full access for less than $1 a day </p>
+          <button type="button" className="btn">
+            Sign Up
+          </button>
+        </div>
+        <div className="foot2">
+          <p className="monthly">Why Us </p>
+          <span className="info">Tutorials by industry experts</span>
+          <span className="info">Peer &amp; expert code review</span>
+          <span className="info"> Coding exercises</span>
+          <span className="info"> Access to our GitHub repos </span>
+          <span className="info">Community forum </span>
+          <span className="info">Flashcard decks</span>
+          <span className="info">New videos every week</span>
+        </div>
+      </div>
+    </>
   );
 }
 
